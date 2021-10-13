80831dc32104be4c41bad715a4919b619bd7ef24
Renata Nunes <renatanunes231@yahoo.com.br>
2021-10-13 17:05:42 -0300

    Desafio 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,17 @@
+// Adicione o campo avaliacao em todos os documentos da coleção e efetue alterações nesse campo.
+db.produtos.updateMany(
+  {},
+  { $set: { avaliacao: NumberInt(0) } },
+);
+
+db.produtos.updateMany(
+  { tags: { $in: ["bovino"] } },
+  { $inc: { avaliacao: 5 } },
+);
+
+db.produtos.updateMany(
+  { tags: { $in: ["ave"] } },
+  { $inc: { avaliacao: 3 } },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, avaliacao: 1 });
