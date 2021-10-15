6790f1fca3c50b809a7b6218687f228406db9482
Renata Nunes <renatanunes231@yahoo.com.br>
2021-10-15 19:12:33 -0300

    Desafios 6, 7, 8 e 9

---
+++ challenges/desafio8.js
@@ -0,0 +1,7 @@
+// Remova o primeiro ingrediente do sanduíche Quarteirão com Queijo.
+db.produtos.updateOne(
+  { nome: "Quarteirão com Queijo" },
+  { $pop: { ingredientes: -1 } },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
---
+++ challenges/desafio9.js
@@ -0,0 +1,7 @@
+// Remova o último ingrediente do sanduíche Cheddar McMelt.
+db.produtos.updateOne(
+  { nome: "Cheddar McMelt" },
+  { $pop: { ingredientes: 1 } },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
