327df42d55473fefc3b4a97bab10cd076337df37
Renata Nunes <renatanunes231@yahoo.com.br>
2021-10-16 16:15:07 -0300

    Desafio 20

---
+++ challenges/desafio20.js
@@ -0,0 +1,7 @@
+// Remova o campo curtidas do item Big Mac.
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  { $unset: { curtidas: "" } },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, curtidas: 1 });
